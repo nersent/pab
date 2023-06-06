73bda6bae9df411526568deae48ddb3db3636d78
xnerhu <xnerhu@gmail.com>
2023-06-07 01:31:09 +0200

    redirect to login page on /basket

--- pab/src/main/java/com/nersent/pab/product/ProductUIController.java
+++ pab/src/main/java/com/nersent/pab/product/ProductUIController.java
@@ -1,17 +1,34 @@
 package com.nersent.pab.product;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.nersent.pab.auth.AuthService;
+import com.nersent.pab.user.UserEntity;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProductUIController {
+  @Autowired
+  private AuthService authService;
+
   @GetMapping("/product")
   public String product() {
     return "product";
   }
 
   @GetMapping("/basket")
-  public String basket() {
+  public Object basket(HttpServletRequest req) {
+    Optional<UserEntity> user = authService.getUserFromReq(req);
+
+    if (!user.isPresent()) {
+      return new RedirectView("/auth/login");
+    }
     return "basket";
   }
 }
